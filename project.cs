de95989551d52f4cfc81da4687d5624b5edbc994
Ugonz86 <ugonzalez86@gmail.com>
2019-07-16 10:10:37 -0700

    This will commit all recent additions

--- project.cs
+++ project.cs
@@ -1,9 +1,38 @@
 using System;
 
-class ProjectName
+class TriangleTracker
 {
   static void Main()
   {
-    
-  }
+    Console.WriteLine("Enter the first angle:");
+    string stringAngleX = Console.ReadLine();
+    int angleX = int.Parse(stringAngleX);
+
+    Console.WriteLine("Enter the second angle:");
+    string stringAngleY = Console.ReadLine();
+    int angleY = int.Parse(stringAngleY);
+
+    Console.WriteLine("Enter the third angle:");
+    string stringAngleZ = Console.ReadLine();
+    int angleZ = int.Parse(stringAngleZ);
+
+    if (angleX == angleY && angleX == angleZ)
+
+    {
+      Console.WriteLine("That is an equilateral triangle!");
+    }
+    else if ((angleX + angleY > angleZ && angleX + angleZ > angleY && angleY + angleZ > angleX) && (angleX == angleY || angleX == angleZ || angleY == angleZ))
+    {
+      Console.WriteLine("That is an isosceles triangle!");
+    }
+    else if ((angleX + angleY > angleZ && angleX + angleZ > angleY && angleY + angleZ > angleX) && !(angleX == angleY || angleX == angleZ || angleY == angleZ))
+    {
+      Console.WriteLine("This is a scalene triangle!");
+    }
+    else
+    {
+      Console.WriteLine("This is NOT a triangle!");
+    }
+
+    }
 }
